09bf00b15dd0d9f134d24b9e8783409e4e170b03
Brian <ndanumumo93@gmail.com>
2025-11-19 14:34:22 +0300

    mumo

---
+++ Desktop/BMBTRADERSTOOL-master/packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '112060', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '112060',
+    'app.deriv.com': '112060',
+    'staging-app.deriv.com': '112060',
+    'goon-puce.vercel.app': '112060',
+});
+
+export default AppIds;
